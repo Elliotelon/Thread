b41dd39fc9ae5c186e0c4cc8735258de511b8851
minkyu <elliotelon@gmail.com>
2026-03-06 23:31:02 +0900

    초기설정 gitignore

---
+++ Thread/ThreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadApp.swift
+//  Thread
+//
+//  Created by 김민규 on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ThreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
