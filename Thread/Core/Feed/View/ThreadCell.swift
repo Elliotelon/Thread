bf48b72ca892839a8f57a5e48d3946243582ea5d
minkyu <elliotelon@gmail.com>
2026-03-11 14:44:10 +0900

    feat: 피드 화면 UI 구현

---
+++ Thread/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,85 @@
+//
+//  ThreadCell.swift
+//  Thread
+//
+//  Created by 김민규 on 3/11/26.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12 ) {
+                Image("person-1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("person-1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                            
+                        }
+
+                    }
+                    
+                    Text("champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+
+                    }
+                    .foregroundStyle(Color(.black))
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
--- Thread/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
+++ Thread/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -17,3 +17,4 @@ struct ThreadsTextFieldModifier: ViewModifier {
             .padding(.horizontal, 24)
     }
 }
+
